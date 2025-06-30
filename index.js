549635546bbaca0f1282e470cc032dea1c6e2ff4
FaisalHakim223 <officialhakim78@gmail.com>
2025-06-30 14:06:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>My Love Anime List</title>
-      </Head>
-      <main style={{ background: '#fff', color: '#000', textAlign: 'center', padding: '50px' }}>
-        <h1 style={{ fontSize: '2.5em' }}>🖤 My Love Anime List</h1>
-        <p style={{ fontSize: '1.2em' }}>あきらめないのがおれのまほうだ</p>
-        <img src="https://upload.wikimedia.org/wikipedia/en/0/03/Asta_Black_Clover.png" alt="Asta" width="200" />
-        <ul style={{ marginTop: '30px', fontSize: '1.1em' }}>
-          <li>Black Clover</li>
-          <li>Tambah anime lain nanti ya bro 💪</li>
-        </ul>
-      </main>
-    </>
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Daftar Anime & Manga Favorit</h1>
+      <p>Selamat datang di list-fav-animanga buatan Faisal!</p>
+    </div>
   );
 }
